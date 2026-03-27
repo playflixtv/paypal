8ab92c5860b39c5cf58bfc590237ee3c3192c5fb
Gustavo <55208546+gustavovalle23@users.noreply.github.com>
2026-03-27 11:16:40 -0300

    chore: remove useless dist folder

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { PayPal } from "./paypal-api";
-export { CreatePlanPayload, CreateSubscriptionPayload, ProductPayload, } from "./types";
-//# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PayPal = void 0;
-var paypal_api_1 = require("./paypal-api");
-Object.defineProperty(exports, "PayPal", { enumerable: true, get: function () { return paypal_api_1.PayPal; } });
-//# sourceMappingURL=index.js.map
